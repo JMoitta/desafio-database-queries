43e296cad8dadb27c0cb0562edf10dc7f43795fb
Joao Moita Manrique <joaomoitamanrique@gmail.com.br>
2022-01-03 16:50:29 -0300

    Desafio

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,33 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({
+      id: user_id
+    }, {
+      relations: ['games']
+    });
+    if (!user) {
+      return {} as User;
+    }
+    user.games;
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY first_name ASC");
+    // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      'SELECT * FROM users WHERE lower(first_name) = $1 AND lower(last_name) = $2',
+      [
+        first_name.toLowerCase(),
+        last_name.toLowerCase()
+      ]);
+    // Complete usando raw query
   }
 }
